add843ddf2f2b66e1aafd0018806afc9dbc8673f
Kayk334534 <158586402+Kayk334534@users.noreply.github.com>
2024-04-12 11:41:37 -0300

    versão 3

--- transmitir.php
+++ transmitir.php
@@ -1,6 +1,13 @@
 <?php
     if($_SERVER["REQUEST_METHOD"] == "POST"){
+
         $email = $_POST['email'];
-        echo($email);
+        $senha = $_POST['senha'];
+
+        if($email == 'kayk@gmail.com' and $senha == '12345'){
+            echo("é isso aí");
+        }else{
+            echo("cai fora");
+        }
     }
 ?>
