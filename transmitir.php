d17bf4aa077c934ae957ade3e751c14bf413c716
Kayk334534 <158586402+Kayk334534@users.noreply.github.com>
2024-04-19 10:48:26 -0300

    Update transmitir.php

--- transmitir.php
+++ transmitir.php
@@ -1,13 +1,15 @@
 <?php
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $email = $_POST['email'];
         $senha = $_POST['senha'];
 
-        if($email == 'kayk@gmail.com' and $senha == '12345'){
-            echo("é isso aí");
+        //and $senha == '12345'
+        if ($email == 'aluno@dominio.com' and md5($senha) == 'e10adc3949ba59abbe56e057f20f883e'){
+            echo("Login realizado com sucesso! a senha digitada é: ".$senha);
+            echo("<p>A senha criptografada em md5 é: ".md5($senha));
         }else{
-            echo("cai fora");
+            echo("Login incorreto!");
         }
+        
     }
 ?>
